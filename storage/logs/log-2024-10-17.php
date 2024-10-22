518137e6926498833eb2bed721666fe0da190145
Dario Rodriguez <dariojrodriguez2000@gmail.com>
2024-10-21 23:36:08 -0300

    Contenido de la V2main - easyappoiment v1.5

---
+++ storage/logs/log-2024-10-17.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-10-17 02:32:47 --> Severity: error --> Exception: Call to undefined function mysqli_init() C:\nginx\html\www.php.com\turno\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-10-17 02:32:52 --> Severity: error --> Exception: Call to undefined function mysqli_init() C:\nginx\html\www.php.com\turno\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-10-17 02:33:29 --> Severity: error --> Exception: Call to undefined function mysqli_init() C:\nginx\html\www.php.com\turno\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-10-17 02:34:08 --> Severity: error --> Exception: Call to undefined function mysqli_init() C:\nginx\html\www.php.com\turno\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-10-17 02:38:36 --> Severity: error --> Exception: Call to undefined function mysqli_init() C:\nginx\html\www.php.com\turno\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-10-17 02:38:50 --> Severity: error --> Exception: Call to undefined function mysqli_init() C:\nginx\html\www.php.com\turno\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-10-17 02:42:44 --> Severity: error --> Exception: Call to undefined function mysqli_init() C:\nginx\html\www.php.com\turno\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-10-17 02:50:02 --> Severity: error --> Exception: Call to undefined function mysqli_init() C:\nginx\html\www.php.com\turno\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2024-10-17 02:54:24 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo for mysql failed: Host desconocido.  C:\nginx\html\www.php.com\turno\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-10-17 02:54:24 --> Severity: error --> Exception: php_network_getaddresses: getaddrinfo for mysql failed: Host desconocido.  C:\nginx\html\www.php.com\turno\system\database\drivers\mysqli\mysqli_driver.php 203
---
+++ storage/logs/log-2024-10-20.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-10-20 03:48:26 --> Could not find the language line "trigger_sync_hint"
+ERROR - 2024-10-20 03:56:04 --> Could not find the language line "trigger_sync_hint"
+ERROR - 2024-10-20 03:57:23 --> Could not find the language line "trigger_sync_hint"
+ERROR - 2024-10-20 04:03:03 --> Could not find the language line "trigger_sync_hint"
+ERROR - 2024-10-20 04:05:02 --> Could not find the language line "trigger_sync_hint"
+ERROR - 2024-10-20 04:05:38 --> Could not find the language line "trigger_sync_hint"
